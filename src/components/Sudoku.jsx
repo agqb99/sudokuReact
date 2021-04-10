62e94b2fc3b4a10c2fe44bdd843be0d7a611ca5e
agqb99 <agqb99@gmail.com>
2021-04-09 22:40:55 -0600

    Agrego las funciones para validar el tablero de 3x3, que no se repitan los valores, que se puedan eliminar,  no permite dejar campos vacios y agrego un boton para que el usuario termine la partida

--- src/components/Sudoku.jsx
+++ src/components/Sudoku.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Sudoku = (props) => {
-  const [tablero, setTablero] = React.useState([
+  const inicialTablero = [
     [
       { value: "", enabled: true },
       { value: "", enabled: true },
@@ -17,7 +17,8 @@ const Sudoku = (props) => {
       { value: "", enabled: true },
       { value: "", enabled: true },
     ],
-  ]);
+  ];
+  const [tablero, setTablero] = React.useState(inicialTablero);
 
   // console.log("Hola");
 
@@ -43,27 +44,84 @@ const Sudoku = (props) => {
           );
         })}
       </div>
+      <div className="finIntento">
+        <button id="FinIntento" type="submit" onClick={checkTablero}>
+          Finalizar Partida
+        </button>
+      </div>
     </>
   );
+
+  // function hasDuplicateInRows() {}
+  // function hasDuplicateInColumns() {}
+
+  function hasEmptyCells() {
+    const returnArray = tablero.flat().map((e) => {
+      return e.value;
+    });
+    const empty = returnArray.some((e) => {
+      return e === "";
+    });
+    return empty;
+  }
+
+  function hasDuplicate() {
+    const returnArray = tablero.flat().map((e) => {
+      return e.value;
+    });
+    const tableroSet = new Set(returnArray);
+    console.log(tableroSet.size);
+    console.log(returnArray.length);
+
+    if (returnArray.length === tableroSet.size) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function cleanCells() {
+    setTablero(inicialTablero);
+  }
+
+  function checkTablero() {
+    if (hasDuplicate()) {
+      // console.log("Valores repetidos");
+      alert("Valores repetidos");
+    } else {
+      alert("No hay valores repetidos");
+      cleanCells();
+    }
+    if (hasEmptyCells()) {
+      alert("No podes dejar valores vacios");
+    }
+  }
 
   function handleChange(e) {
     const indiceFila = parseInt(e.target.name.charAt(0));
     const indiceColumna = parseInt(e.target.name.charAt(2));
-
-    // console.log(e.target.value);
-    setTablero(
-      tablero.map((columna, i) => {
-        return columna.map((celda, j) => {
-          if (i === indiceFila && j === indiceColumna) {
-            return {
-              ...celda,
-              value: parseInt(e.target.value),
-            };
-          }
-          return celda;
-        });
-      })
-    );
+    const validacionDigito = /^\d?$/;
+    if (validacionDigito.test(e.target.value)) {
+      // console.log(e.target.value);
+      setTablero(
+        tablero.map((columna, i) => {
+          return columna.map((celda, j) => {
+            if (i === indiceFila && j === indiceColumna) {
+              return {
+                ...celda,
+                value: isNaN(parseInt(e.target.value))
+                  ? ""
+                  : parseInt(e.target.value),
+              };
+            }
+            return celda;
+          });
+        })
+      );
+      // if (e.target.value === "" ) {
+      //   alert("No puedes dejar espacios en blanco")
+      // }
+    }
   }
 };
 
