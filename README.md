c59f2201cc9fbca926b62e3932bd3045a2504e37
agqb99 <agqb99@gmail.com>
2021-04-15 00:24:30 -0600

    Se terminan de pasar los valores al component de Scores

--- README.md
+++ README.md
@@ -5,15 +5,15 @@
         - [] Reglas del Sudoku
         - [X] Registro del user
 
-    2. [] Crear Grid con valores cargados (3 numeros).
+    2. [X] Crear Grid con valores cargados (3 numeros).
         - [X] Tablero 3x3
         - [X] Verificar que el numero ingresado no se repita.
         - [X] Timer
 
-    3. [] Tabla de puntajes.
+    3. [X] Tabla de puntajes.
         - [X] Crear component de Score.
         - [X] Pasar PlayerName.
-        - [] Pasar Score de los users.
+        - [X] Pasar Score de los users.
 
     4. [] Agregar Styles a los components.
 
--- src/components/Scores.jsx
+++ src/components/Scores.jsx
@@ -1,12 +1,37 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 const Scores = (props) => {
+  const history = useHistory();
+
   return (
-    <div className="homeInformacion">
+    <div className="">
       <h2>Scores</h2>
-      <h2>Jugador: {props.playerName}</h2>
+      {/* <h2>Jugador: {props.playerName}</h2> */}
+      {props.users.map((user) => (
+        <div>
+          <div className="usuarios">
+            <h4>Jugador: {user.playerName}</h4>
+            <h4>Tiempo: {user.tiempo}</h4>
+          </div>
+          <div className="otro">
+            <button id="home" type="submit" onClick={handlerBoton}>
+              Volver al Inicio
+            </button>
+            <button id="otroIntento" type="submit" onClick={handlerBoton2}>
+              Jugar de nuevo
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
   );
+  function handlerBoton2() {
+    history.push("/sudoku");
+  }
+  function handlerBoton() {
+    history.push("/");
+  }
 };
 
 export default Scores;
